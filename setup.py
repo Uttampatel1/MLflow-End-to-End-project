8b2d0b683ecf4279799c65fb66949433f031f7a6
Uttam patel <utampipaliya@gmail.com>
2023-07-30 11:44:15 +0530

    add setup file

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-with-MLflow"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "MLflow-End-to-End-project"
+AUTHOR_USER_NAME = "Uttampatel1"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "utampipaliya@gmail.com"
 
 
 setuptools.setup(
